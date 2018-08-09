73e065b75a4b90ed1a93be161312a0feb3ff191c
Petr Muller <muller@redhat.com>
2018-08-09 18:05:10 +0200

    Add additional job to build [images]

--- tools/ci-operator-prowgen/main.go
+++ tools/ci-operator-prowgen/main.go
@@ -63,16 +63,21 @@ func generatePodSpec(org, repo, branch string) *kubeapi.PodSpec {
 	}
 }
 
-func generatePresubmitForTest(test, org, repo, branch string) *prowconfig.Presubmit {
+type testDescription struct {
+	Name   string
+	Target string
+}
+
+func generatePresubmitForTest(test testDescription, org, repo, branch string) *prowconfig.Presubmit {
 	presubmit := prowconfig.Presubmit{
 		Agent:        "kubernetes",
 		AlwaysRun:    true,
 		Brancher:     prowconfig.Brancher{Branches: []string{branch}},
-		Context:      fmt.Sprintf("ci/prow/%s", test),
-		Name:         fmt.Sprintf("pull-ci-%s-%s-%s", org, repo, test),
-		RerunCommand: fmt.Sprintf("/test %s", test),
+		Context:      fmt.Sprintf("ci/prow/%s", test.Name),
+		Name:         fmt.Sprintf("pull-ci-%s-%s-%s", org, repo, test.Name),
+		RerunCommand: fmt.Sprintf("/test %s", test.Name),
 		Spec:         generatePodSpec(org, repo, branch),
-		Trigger:      fmt.Sprintf("((?m)^/test( all| %s),?(\\\\s+|$))", test),
+		Trigger:      fmt.Sprintf("((?m)^/test( all| %s),?(\\\\s+|$))", test.Name),
 		UtilityConfig: prowconfig.UtilityConfig{
 			DecorationConfig: &prowkube.DecorationConfig{SkipCloning: true},
 			Decorate:         true,
@@ -80,16 +85,16 @@ func generatePresubmitForTest(test, org, repo, branch string) *prowconfig.Presub
 	}
 	presubmit.Spec.Containers[0].Args = append(
 		presubmit.Spec.Containers[0].Args,
-		fmt.Sprintf("--target=%s", test),
+		fmt.Sprintf("--target=%s", test.Target),
 	)
 
 	return &presubmit
 }
 
-func generatePostsubmitForTest(test, org, repo, branch string) *prowconfig.Postsubmit {
+func generatePostsubmitForTest(test testDescription, org, repo, branch string) *prowconfig.Postsubmit {
 	postsubmit := prowconfig.Postsubmit{
 		Agent: "kubernetes",
-		Name:  fmt.Sprintf("branch-ci-%s-%s-%s", org, repo, test),
+		Name:  fmt.Sprintf("branch-ci-%s-%s-%s", org, repo, test.Name),
 		Spec:  generatePodSpec(org, repo, branch),
 		UtilityConfig: prowconfig.UtilityConfig{
 			DecorationConfig: &prowkube.DecorationConfig{SkipCloning: true},
@@ -99,30 +104,42 @@ func generatePostsubmitForTest(test, org, repo, branch string) *prowconfig.Posts
 
 	postsubmit.Spec.Containers[0].Args = append(
 		postsubmit.Spec.Containers[0].Args,
-		fmt.Sprintf("--target=%s", test),
+		fmt.Sprintf("--target=%s", test.Target),
 	)
 	postsubmit.Spec.Containers[0].Args = append(postsubmit.Spec.Containers[0].Args, "--promote")
 
 	return &postsubmit
 }
 
-func generatePresubmits(tests []cioperatorapi.TestStepConfiguration, org, repo, branch string) map[string][]prowconfig.Presubmit {
+func generateJobs(
+	configSpec *cioperatorapi.ReleaseBuildConfiguration,
+	org, repo, branch string,
+) (*map[string][]prowconfig.Presubmit, *map[string][]prowconfig.Postsubmit) {
+
 	orgrepo := fmt.Sprintf("%s/%s", org, repo)
 	presubmits := map[string][]prowconfig.Presubmit{}
-	for _, element := range tests {
-		presubmits[orgrepo] = append(presubmits[orgrepo], *generatePresubmitForTest(element.As, org, repo, branch))
-	}
-	return presubmits
-}
-
-func generatePostsubmits(tests []cioperatorapi.TestStepConfiguration, org, repo, branch string) map[string][]prowconfig.Postsubmit {
-	orgrepo := fmt.Sprintf("%s/%s", org, repo)
 	postsubmits := map[string][]prowconfig.Postsubmit{}
-	for _, element := range tests {
-		postsubmits[orgrepo] = append(postsubmits[orgrepo], *generatePostsubmitForTest(element.As, org, repo, branch))
+
+	imagesTest := false
+
+	for _, element := range configSpec.Tests {
+		// Check if config file has "images" test defined to avoid name clash
+		if element.As == "images" {
+			imagesTest = true
+		}
+		test := testDescription{Name: element.As, Target: element.As}
+		presubmits[orgrepo] = append(presubmits[orgrepo], *generatePresubmitForTest(test, org, repo, branch))
+		postsubmits[orgrepo] = append(postsubmits[orgrepo], *generatePostsubmitForTest(test, org, repo, branch))
 	}
 
-	return postsubmits
+	if len(configSpec.Images) > 0 && !imagesTest {
+		// TODO: somehow handle the images case better than just not creating this job when there is name conflict
+		test := testDescription{Name: "images", Target: "[images]"}
+		presubmits[orgrepo] = append(presubmits[orgrepo], *generatePresubmitForTest(test, org, repo, branch))
+		postsubmits[orgrepo] = append(postsubmits[orgrepo], *generatePostsubmitForTest(test, org, repo, branch))
+	}
+
+	return &presubmits, &postsubmits
 }
 
 func main() {
@@ -156,9 +173,11 @@ func main() {
 	org := path.Base(path.Dir(configSpecDir))
 	branch := strings.TrimSuffix(path.Base(opt.ciOperatorConfigPath), path.Ext(opt.ciOperatorConfigPath))
 
+	presubmits, postsubmits := generateJobs(configSpec, org, repo, branch)
+
 	jobConfig := prowconfig.JobConfig{
-		Presubmits:  generatePresubmits(configSpec.Tests, org, repo, branch),
-		Postsubmits: generatePostsubmits(configSpec.Tests, org, repo, branch),
+		Presubmits:  *presubmits,
+		Postsubmits: *postsubmits,
 	}
 
 	jobConfigAsYaml, err := yaml.Marshal(jobConfig)
