4b626a643b6eb456c0df563014ae0974eeffa7cb
Petr Muller <muller@redhat.com>
2018-08-09 15:06:25 +0200

    Address rewiew, improve formatting

--- tools/ci-operator-prowgen/main.go
+++ tools/ci-operator-prowgen/main.go
@@ -14,6 +14,7 @@ import (
 	cioperatorapi "github.com/openshift/ci-operator/pkg/api"
 	kubeapi "k8s.io/api/core/v1"
 	prowconfig "k8s.io/test-infra/prow/config"
+	prowkube "k8s.io/test-infra/prow/kube"
 )
 
 type options struct {
@@ -33,49 +34,63 @@ func bindOptions(flag *flag.FlagSet) *options {
 	return opt
 }
 
-func generatePodSpec(org, component, branch string) *kubeapi.PodSpec {
+func generatePodSpec(org, repo, branch string) *kubeapi.PodSpec {
 	podspec := kubeapi.PodSpec{ServiceAccountName: "ci-operator"}
 	command := []string{"ci-operator"}
 	args := []string{"--artifact-dir=$(ARTIFACTS)"}
 
-	configMapKeyRef := kubeapi.EnvVarSource{ConfigMapKeyRef: &kubeapi.ConfigMapKeySelector{LocalObjectReference: kubeapi.LocalObjectReference{Name: fmt.Sprintf("ci-operator-%s-%s", org, component)}, Key: fmt.Sprintf("%s.json", branch)}}
-	configSpec := kubeapi.EnvVar{Name: "CONFIG_SPEC"}
+	configMapKeyRef := kubeapi.EnvVarSource{
+		ConfigMapKeyRef: &kubeapi.ConfigMapKeySelector{
+			LocalObjectReference: kubeapi.LocalObjectReference{Name: fmt.Sprintf("ci-operator-%s-%s", org, repo)},
+			Key:                  fmt.Sprintf("%s.json", branch),
+		},
+	}
+	configSpec := kubeapi.EnvVar{Name: "CONFIG_SPEC", ValueFrom: &configMapKeyRef}
 	env := []kubeapi.EnvVar{configSpec}
 
-	container := kubeapi.Container{Name: "test", Image: "ci-operator:latest", Command: command, Args: args, Env: env}
+	container := kubeapi.Container{
+		Name:    "test",
+		Image:   "ci-operator:latest",
+		Command: command,
+		Args:    args,
+		Env:     env,
+	}
 	podspec.Containers = []kubeapi.Container{container}
 
 	return &podspec
 }
 
-func generatePresubmitForTest(test, org, component, branch string) prowconfig.Presubmit {
-	presubmit := prowconfig.Presubmit{Agent: "kubernetes", AlwaysRun: true}
-	presubmit.Name = fmt.Sprintf("ci-operator-%s-%s-%s", org, component, test)
-	presubmit.Context = fmt.Sprintf("ci/prow/%s", test)
-	presubmit.RerunCommand = fmt.Sprintf("/test %s", test)
-	presubmit.Trigger = fmt.Sprintf("((?m)^/test( all| %s),?(\\\\s+|$))", test)
-	presubmit.Brancher = prowconfig.Brancher{}
-	presubmit.Brancher.Branches = []string{branch}
-	presubmit.UtilityConfig = prowconfig.UtilityConfig{Decorate: true}
-	// TODO: `skip_cloning` does not seem to be covered by `Presubmit`
-	presubmit.Spec = generatePodSpec(org, component, branch)
+func generatePresubmitForTest(test, org, repo, branch string) prowconfig.Presubmit {
+	presubmit := prowconfig.Presubmit{
+		Agent:        "kubernetes",
+		AlwaysRun:    true,
+		Brancher:     prowconfig.Brancher{Branches: []string{branch}},
+		Context:      fmt.Sprintf("ci/prow/%s", test),
+		Name:         fmt.Sprintf("ci-operator-%s-%s-%s", org, repo, test),
+		RerunCommand: fmt.Sprintf("/test %s", test),
+		Spec:         generatePodSpec(org, repo, branch),
+		Trigger:      fmt.Sprintf("((?m)^/test( all| %s),?(\\\\s+|$))", test),
+		UtilityConfig: prowconfig.UtilityConfig{
+			DecorationConfig: &prowkube.DecorationConfig{SkipCloning: true},
+			Decorate:         true,
+		},
+	}
 
 	return presubmit
 }
 
-func generatePresubmits(tests []cioperatorapi.TestStepConfiguration, org, component, branch string) map[string][]prowconfig.Presubmit {
-	repo := fmt.Sprintf("%s/%s", org, component)
-	presubmits := make(map[string][]prowconfig.Presubmit)
-	presubmits[repo] = make([]prowconfig.Presubmit, 0)
+func generatePresubmits(tests []cioperatorapi.TestStepConfiguration, org, repo, branch string) map[string][]prowconfig.Presubmit {
+	orgrepo := fmt.Sprintf("%s/%s", org, repo)
+	presubmits := map[string][]prowconfig.Presubmit{}
 	for _, element := range tests {
-		presubmits[repo] = append(presubmits[repo], generatePresubmitForTest(element.As, org, component, branch))
+		presubmits[orgrepo] = append(presubmits[orgrepo], generatePresubmitForTest(element.As, org, repo, branch))
 	}
 	return presubmits
 }
 
-func generateJobConfig(configSpec *cioperatorapi.ReleaseBuildConfiguration, org, component, branch string) *prowconfig.JobConfig {
+func generateJobConfig(configSpec *cioperatorapi.ReleaseBuildConfiguration, org, repo, branch string) *prowconfig.JobConfig {
 	jobConfig := prowconfig.JobConfig{}
-	jobConfig.Presubmits = generatePresubmits(configSpec.Tests, org, component, branch)
+	jobConfig.Presubmits = generatePresubmits(configSpec.Tests, org, repo, branch)
 	// TODO: postsubmits
 	// jobConfig.Postsubmits = make(map[string][]prowconfig.Postsubmit)
 	// jobConfig.Postsubmits[repo] = make([]prowconfig.Postsubmit, 0)
@@ -111,11 +126,11 @@ func main() {
 	}
 
 	configSpecDir := path.Dir(opt.ciOperatorConfigPath)
-	component := path.Base(configSpecDir)
+	repo := path.Base(configSpecDir)
 	org := path.Base(path.Dir(configSpecDir))
 	branch := strings.TrimSuffix(path.Base(opt.ciOperatorConfigPath), path.Ext(opt.ciOperatorConfigPath))
 
-	jobConfig := generateJobConfig(configSpec, org, component, branch)
+	jobConfig := generateJobConfig(configSpec, org, repo, branch)
 
 	jobConfigAsYaml, err := yaml.Marshal(jobConfig)
 	if err != nil {
